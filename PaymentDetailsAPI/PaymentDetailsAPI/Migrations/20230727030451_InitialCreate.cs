e8044601878daabdf804ac6ec5c2bf0955574e04
brunocarmena57 <brunorcarmena1@gmail.com>
2023-08-01 00:41:37 -0300

    first commit

---
+++ PaymentDetailsAPI/PaymentDetailsAPI/Migrations/20230727030451_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaymentDetailsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDetails",
+                columns: table => new
+                {
+                    PaymentDetailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CardOwnerName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    CardNumber = table.Column<string>(type: "nvarchar(16)", nullable: true),
+                    ExpirationDate = table.Column<string>(type: "nvarchar(5)", nullable: true),
+                    SecurityCode = table.Column<string>(type: "nvarchar(3)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetails", x => x.PaymentDetailId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDetails");
+        }
+    }
+}
---
+++ PaymentDetailsAPI/PaymentDetailsAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentDetailsAPI.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
